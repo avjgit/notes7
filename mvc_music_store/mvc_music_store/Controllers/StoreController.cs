92802ca83cf93cb213465058a1f53fb8f083bbda
avjgit <avjmail@gmail.com>
2014-05-30 23:11:44 +0300

    2/10 notes

--- mvc_music_store/mvc_music_store/Controllers/StoreController.cs
+++ mvc_music_store/mvc_music_store/Controllers/StoreController.cs
@@ -16,12 +16,19 @@ namespace mvc_music_store.Controllers
             return "show'em all";
         }
 
+        //post parameters named “genre” will be automatically past
         public string Browse(string genre)
         {
             string message = HttpUtility.HtmlEncode("browsing genre " + genre);
             return message;
         }
+        //HttpUtility.HtmlEncode utility to sanitize the user input. 
+        //This prevents users from injecting Javascript into our View 
+        //with a link like /Store/Browse?Genre=<script>window.location=’http://hackersite.com’</script>
 
+        // ASP.NET MVC’s default routing convention is 
+        //to treat the segment of a URL after the action method name 
+        //as a parameter named “ID”
         public string Details(int id)
         {
             string message = HttpUtility.HtmlEncode("details for " + id);
