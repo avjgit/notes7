91b8d781da621ba0156e7e6a4e6aa75c787e2365
avjgit <avjmail@gmail.com>
2014-05-31 09:09:13 +0300

    3/10 checking

---
+++ mvc_music_store/mvc_music_store/Controllers/AsdfController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_music_store.Controllers
+{
+    public class AsdfController : Controller
+    {
+        //
+        // GET: /Asdf/
+
+        public ActionResult ShowThis()
+        {
+            return View();
+        }
+
+    }
+}
