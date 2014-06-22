8aa469092511870936f66a8e933f165f171a00e3
AJ <avjmail@gmail.com>
2014-06-22 21:17:41 +0300

    8/10 viewmodels

---
+++ mvc_music_store/mvc_music_store/Controllers/ShoppingCartController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using mvc_music_store.Models;
+using mvc_music_store.ViewModels;
+
+namespace mvc_music_store.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+        StoreRecords db = new StoreRecords();
+        //
+        // GET: /ShoppingCart/
+
+        public ActionResult Index()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            var viewModel = new ShoppingCartViewModel
+            {
+                CartItems = cart.GetCartItems(),
+                CartTotal = cart.GetTotal()
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult AddToCart(int id)
+        {
+            var addedAlbum = db.Albums.Single(album => album.AlbumId == id);
+
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            cart.AddToCart(addedAlbum);
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RemoveFromCart(int id)
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+
+            string albumName = db.Carts.Single(item => item.RecordId == id).Album.Title;
+
+            int itemCount = cart.RemoveFromCart(id);
+
+            var results = new ShoppingCartRemoveViewModel
+            {
+                Message = Server.HtmlEncode(albumName) + " has been removed from your shopping cart.",
+                CartTotal = cart.GetTotal(),
+                CartCount = cart.GetCount(),
+                ItemCount = itemCount,
+                DeleteId = id
+            };
+            return Json(results);
+        }
+
+        [ChildActionOnly]
+        public ActionResult CartSummary()
+        {
+            var cart = ShoppingCart.GetCart(this.HttpContext);
+            ViewData["CartCount"] = cart.GetCount();
+            return PartialView("CartSummary");
+        }
+
+    }
+}
---
+++ mvc_music_store/mvc_music_store/ViewModels/ShoppingCartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using mvc_music_store.Models;
+
+namespace mvc_music_store.ViewModels
+{
+    public class ShoppingCartViewModel
+    {
+        public List<Cart> CartItems { get; set; }
+        public decimal CartTotal { get; set; }
+    }
+}
