464eb96aab069666e0ed8402843ba4b1beab4f73
AJ <avjmail@gmail.com>
2014-06-22 22:26:02 +0300

    controller

---
+++ mvc_5_intro/mvc_5_intro/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc_5_intro.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+        public string Index()
+        {
+            return "this is default action";
+        }
+
+        public string Welcome(string name, int numtimes = 1)
+        {
+            return "this is welcome";
+        }
+	}
+}
