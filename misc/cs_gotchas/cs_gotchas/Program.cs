ed850bba0412f6d68e6924013416dc3ae0b8a702
AJ <avjmail@gmail.com>
2014-07-03 23:51:10 +0300

    ref vs value

--- misc/cs_gotchas/cs_gotchas/Program.cs
+++ misc/cs_gotchas/cs_gotchas/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace cs_gotchas
 {
@@ -10,8 +11,26 @@ namespace cs_gotchas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            
+            ////////////////////////////////////////////////////////////////// ref vs values
+            Point point1 = new Point(10, 20);
+            Point point2 = point1;
+            point2.X = 30; //ok, still 10
+            Console.WriteLine(point1.X);
+
+            Pen pen1 = new Pen(Color.Black);
+            Pen pen2 = pen1;
+            pen2.Color = Color.Red;
+            Console.WriteLine(pen1.Color); //red!
+
+            //point1 and point2 each contain their own copy of a Point object, 
+            //whereas pen1 and pen2 contain references to the same Pen object.
+            //public struct Point { … }     // defines a “value” type
+            //public class Pen { … }        // defines a “reference” type   
+
+
             Console.ReadLine();
         }
+
     }
 }
