d1cc79219d08eb4081ad08d8d3f007ee15238891
AJ <avjmail@gmail.com>
2014-07-09 20:13:19 +0300

    tutorialspoint notes

---
+++ misc/cs_tutorialspoint/cs_tutorialspoint/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs_tutorialspoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // what? dynamic types?
+            dynamic x = 20;
+            x = "a";
+
+            // public, private
+            // protected - available for children only
+            // internal - available to other in same assembly
+            Console.ReadLine();
+
+            //The Null Coalescing Operator (??)
+            double? num1 = null;
+            double num3 = num1 ?? 1;
+        }
+    }
+}
