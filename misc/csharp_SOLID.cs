5b8046962999405114de98a83dab53a8e2411c9d
AJ <avjmail@gmail.com>
2014-07-30 22:28:17 +0300

    SOLID: explanation

---
+++ misc/csharp_SOLID.cs
@@ -0,0 +1,19 @@
+using System;
+
+class Notes
+{
+
+    // reading http://www.codeproject.com/Articles/703634/SOLID-architecture-principles-using-simple-Csharp
+
+    static void Main()
+    {
+
+        // S stands for SRP (Single responsibility principle
+        // O stands for OCP (Open closed principle)
+        // L stands for LSP (Liskov substitution principle)
+        // I stands for ISP ( Interface segregation principle)
+        // D stands for DIP ( Dependency inversion principle)
+        Console.ReadLine();
+    }
+
+}
