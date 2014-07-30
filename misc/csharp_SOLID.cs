5250b4573154a88b43a704d36e29ee97bada159a
AJ <avjmail@gmail.com>
2014-07-30 22:53:21 +0300

    SOLID: Open-closed: problem

--- misc/csharp_SOLID.cs
+++ misc/csharp_SOLID.cs
@@ -29,6 +29,26 @@ class Customer
             logger.Handle(ex.ToString());
         }
     }
+
+    private int _CustType;
+
+    public int CustType
+    {
+        get { return _CustType; }
+        set { _CustType = value; }
+    }
+
+    public double getDiscount(double TotalSales)
+    {
+            if (_CustType == 1)
+            {
+                return TotalSales - 100;
+            }
+            else
+            {
+                return TotalSales - 50;
+            }
+    }
 }
 
 class Notes
