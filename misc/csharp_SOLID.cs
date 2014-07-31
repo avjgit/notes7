453ce6921be773530af7c8147a250949ede55a03
AJ <avjmail@gmail.com>
2014-07-31 22:23:38 +0300

    fixes, cleanup and formatting

--- misc/csharp_SOLID.cs
+++ misc/csharp_SOLID.cs
@@ -1,7 +1,7 @@
 using System;
 
 //
-// S stands for SRP (Single responsibility principle
+// each class has Single responsibility
 //
 class FileLogger
 {
@@ -10,38 +10,38 @@ class FileLogger
         System.IO.File.WriteAllText(@"c:\Error.txt", error);
     }
 }
-class Customer
+
+partial class Customer
 {
     private FileLogger logger = new FileLogger();
 
-    public virtual void Add()
+    public virtual void DoSomething()
     {
-
         try
         {
-            // Database code goes here
+            // some code goes here
         }
         catch (Exception ex)
         {
-            // here Customer class does what it is not supposed to do - logging.
-            // It should be handled by some insance of Logging class
             // System.IO.File.WriteAllText(@"c:\Error.txt", ex.ToString());
+            // here Customer class did what it is not supposed to do - logging.
+            // It should be handled by some insance of Logging class
             logger.Handle(ex.ToString());
         }
     }
+}
 
-    private int _CustType;
+//
+// class should be Open for extensions, Closed for modifications
+//
+partial class Customer
+{
+    public int CustType {get; set;}
 
-    public int CustType
-    {
-        get { return _CustType; }
-        set { _CustType = value; }
-    }
-
-    public double getDiscount(double TotalSales)
-    {
-        // problem: for each new type, new IF should be added -
-        // .. means, class should be modified
+    // problem was: for each new type, new IF should be added -
+    // .. means, class should be modified
+    // public double getDiscount(double TotalSales)
+    // {
         // if (_CustType == 1)
         // {
         //     return TotalSales - 100;
@@ -50,13 +50,14 @@ class Customer
         // {
         //     return TotalSales - 50;
         // }
-        public virtual double getDiscount(double TotalSales)
-        {
-            return TotalSales;
-        }
+    // }
+    public virtual double getDiscount(double TotalSales)
+    {
+        return TotalSales;
     }
 }
 
+// instead of modification, extensions (derived classes) should be created
 class SilverCustomer : Customer
 {
     public override double getDiscount(double TotalSales)
@@ -69,24 +70,21 @@ class GoldCustomer : Customer
 {
     public override double getDiscount(double TotalSales)
     {
-        return base.getDiscount(TotalSales) = 100;
+        return base.getDiscount(TotalSales) - 100;
     }
 }
 
 class Notes
 {
-
     // reading http://www.codeproject.com/Articles/703634/SOLID-architecture-principles-using-simple-Csharp
 
     static void Main()
     {
-
-        // S stands for SRP (Single responsibility principle
+        // S stands for SRP (Single responsibility principle)
         // O stands for OCP (Open closed principle)
         // L stands for LSP (Liskov substitution principle)
-        // I stands for ISP ( Interface segregation principle)
-        // D stands for DIP ( Dependency inversion principle)
+        // I stands for ISP (Interface segregation principle)
+        // D stands for DIP (Dependency inversion principle)
         Console.ReadLine();
     }
-
 }
