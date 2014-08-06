302a07d31a86da4e601f0c7482d0857349425010
AJ <avjmail@gmail.com>
2014-08-06 23:00:48 +0300

    ng misc

---
+++ angularjs/misc.js
@@ -0,0 +1,5 @@
+// http://wintellect.com/blogs/jlikness/10-reasons-web-developers-should-learn-angularjs
+
+// example of app:
+<script src="http://code.angularjs.org/1.2.9/angular.js"></script>
+<div ng-app><input type="text" ng-model="aaa"/><span>{{aaa}}</span></div>
