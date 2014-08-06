6ed38b732d062cec2d45a58ba2f54570d5d97e8e
AJ <avjmail@gmail.com>
2014-08-06 23:03:10 +0300

    ng level 1 notes

---
+++ angularjs/codeschool.js
@@ -0,0 +1,44 @@
+// source: http://campus.codeschool.com/courses/shaping-up-with-angular-js/level/1/section/1/video/1
+
+
+// LEVEL 1
+// Flatlander's Gem Store
+// 3 Videos - 4 Challenges
+// Get comfortable with expressions by beginning to build a gem store.
+///////////////////////////////////////////////////////////////////////////////
+// easy to test
+// response with json data
+//
+// Directive: a marker in html tag to run js function
+//
+// Modules: pieces of application
+// var app = angular.module('store', []); // in [] goes list of dependencies
+//
+// Expressions: "<p>Answer is {{40 + 2}}</p>"
+
+// LEVEL 2
+// Built-in Directives
+// 2 Videos - 8 Challenges
+// Use directives to add a gallery and tabs to the gem store.
+///////////////////////////////////////////////////////////////////////////////
+
+
+// LEVEL 3
+// Forms
+// 3 Videos - 9 Challenges
+// Give the gem store review functionality by using Angular.js forms.
+///////////////////////////////////////////////////////////////////////////////
+
+
+// LEVEL 4
+// Custom Directives
+// 2 Videos - 10 Challenges
+// Create custom directives for more organized and maintainable Angular.js code.
+///////////////////////////////////////////////////////////////////////////////
+
+
+// LEVEL 5
+// Services
+// 2 Videos - 3 Challenges
+// Create a new module and learn to use the $http service to get real data into the gem store.
+///////////////////////////////////////////////////////////////////////////////
