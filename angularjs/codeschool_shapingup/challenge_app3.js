f541c24796b8693a98c4edb286177116288c48b6
AJ <avjmail@gmail.com>
2014-08-10 20:27:37 +0300

    ch4.6: putting controller into directive

--- angularjs/codeschool_shapingup/challenge_app3.js
+++ angularjs/codeschool_shapingup/challenge_app3.js
@@ -4,7 +4,18 @@
   app.directive('productTabs', function(){
     return{
       restrict: 'E',
-      templateUrl: 'product_tabs.html'
+      templateUrl: 'product_tabs.html',
+      controller: function(){
+        this.tab = 1;
+
+        this.setTab = function(newValue){
+          this.tab = newValue;
+        };
+
+        this.isSet = function(tabName){
+          return this.tab === tabName;
+        };
+      });
     };
   });
 
