b3f40d220ead7d75558b037b8dfefc3306d0c597
AJ <avjmail@gmail.com>
2014-08-16 07:14:48 -0700

    02  model Person

---
+++ aspnetmvc/mvc4jumpstart/mvc4jumpstart/Models/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvc4jumpstart.Models
+{
+    public class Person
+    {
+        public int PersonId { get; set; }   
+
+        [Required(ErrorMessage = "Please enter a name.")]
+        public string Name { get; set; }
+
+        [Range(0,400)]
+        public int Height { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddress { get; set; }
+    }
+}
