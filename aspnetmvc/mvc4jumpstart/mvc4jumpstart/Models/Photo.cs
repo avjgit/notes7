535cdd4630ccef8bf1a6ab9924ce74a0de2e9956
AJ <avjmail@gmail.com>
2014-08-16 07:08:56 -0700

    02 data annotations

--- aspnetmvc/mvc4jumpstart/mvc4jumpstart/Models/Photo.cs
+++ aspnetmvc/mvc4jumpstart/mvc4jumpstart/Models/Photo.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,18 @@ namespace mvc4jumpstart.Models
 {
     public class Photo
     {
-        public int PhotoID { get; set; }
+        public int PhotoId { get; set; }
         public string Title { get; set; }
+        
+        [DisplayName("Picture")] 
         public byte[] PhotoFile { get; set; }
+        
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [DataType(DataType.DateTime)]
+        [DisplayName("CreatedDate")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yy}")]
         public DateTime CreatedDate { get; set; }
         public string Owner { get; set; }
         public virtual List<Comment> Comments { get; set; } 
