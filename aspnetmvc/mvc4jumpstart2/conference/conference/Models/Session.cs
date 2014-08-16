5746b2e8b1804d68af9003f81b167c2cd96ee5d0
AJ <avjmail@gmail.com>
2014-08-16 08:25:21 -0700

    02 models

---
+++ aspnetmvc/mvc4jumpstart2/conference/conference/Models/Session.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace conference.Models
+{
+    public class Session
+    {
+        public int SessionId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        [DataType(DataType.MultilineText)]
+        public string Abstract { get; set; }
+        public int SpeakerId { get; set; }
+        public virtual Speaker Speaker { get; set; }
+    }
+}
---
+++ aspnetmvc/mvc4jumpstart2/conference/conference/Models/Speaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace conference.Models
+{
+    public class Speaker
+    {
+        public int SpeakerId { get; set; }
+        
+        [Required(ErrorMessage = "Speaker name is required")]
+        [Display(Name = "Speaker name")]
+        public string Name { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string EmailAddres { get; set; }
+
+        public virtual List<Session> Sessions { get; set; } 
+    }
+}
