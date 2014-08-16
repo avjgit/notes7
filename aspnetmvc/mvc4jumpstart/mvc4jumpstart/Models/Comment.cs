9489adcec77ee28986b4b5371b6480f1854da7f0
AJ <avjmail@gmail.com>
2014-08-16 06:59:20 -0700

    02 model

---
+++ aspnetmvc/mvc4jumpstart/mvc4jumpstart/Models/Comment.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.AccessControl;
+
+namespace mvc4jumpstart.Models
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string User { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public int PhotoId { get; set; }
+    }
+}
---
+++ aspnetmvc/mvc4jumpstart/mvc4jumpstart/Models/Photo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace mvc4jumpstart.Models
+{
+    public class Photo
+    {
+        public int PhotoID { get; set; }
+        public string Title { get; set; }
+        public byte[] PhotoFile { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Owner { get; set; }
+        public virtual List<Comment> Comments { get; set; } 
+    }
+}
