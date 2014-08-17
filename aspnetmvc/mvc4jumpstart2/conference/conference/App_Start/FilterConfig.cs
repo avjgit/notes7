d22ee86ca4b573330482074f6e051f5361d7a556
AJ <avjmail@gmail.com>
2014-08-17 14:17:50 -0700

    04 outcommented https

--- aspnetmvc/mvc4jumpstart2/conference/conference/App_Start/FilterConfig.cs
+++ aspnetmvc/mvc4jumpstart2/conference/conference/App_Start/FilterConfig.cs
@@ -8,7 +8,7 @@ namespace conference
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
-            filters.Add(new RequireHttpsAttribute());
+            //filters.Add(new RequireHttpsAttribute());
         }
     }
 }
