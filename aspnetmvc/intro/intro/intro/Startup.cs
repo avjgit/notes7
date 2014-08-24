c40724be25f1d77fc5883bb8a9876d24f5581c11
AJ <avjmail@gmail.com>
2014-08-24 09:25:50 +0300

    mvc intro init

---
+++ aspnetmvc/intro/intro/intro/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(intro.Startup))]
+namespace intro
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
