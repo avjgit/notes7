1a39ee4dbd5c5543ebdecddbcb1eabd12e52246d
AJ <avjmail@gmail.com>
2014-08-27 23:08:17 +0300

    xunit test run!
    C:\Users\a.jurcenoks\Documents\GitHub\notes7\xunit\howtouse\howtouse\bin\Debug>x
    unit.console.clr4 howtouse.dll
    xUnit.net console test runner (64-bit .NET 4.0.30319.18444)
    Copyright (C) 2013 Outercurve Foundation.
    
    xunit.dll:     Version 1.9.2.1705
    Test assembly:
    C:\Users\a.jurcenoks\Documents\GitHub\notes7\xunit\howtouse\howto
    use\bin\Debug\howtouse.dll
    
    1 total, 0 failed, 0 skipped, took 0.670 seconds

--- xunit/howtouse/howtouse/MyTest.cs
+++
@@ -1,8 +0,0 @@
-﻿using Xunit;
-
-namespace howtouse
-{
-    public class MyTest
-    {
-    }
-}
---
+++ xunit/howtouse/howtouse/MyTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace howtouse
+{
+    public class MyTests
+    {
+        [Fact]
+        public void MyTest()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
