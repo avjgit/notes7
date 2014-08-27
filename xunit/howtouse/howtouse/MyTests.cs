b1a61a20fb78d06299ca18adef401591a1fc7aad
AJ <avjmail@gmail.com>
2014-08-27 23:16:36 +0300

    xunit expected exception

--- xunit/howtouse/howtouse/MyTests.cs
+++ xunit/howtouse/howtouse/MyTests.cs
@@ -9,5 +9,25 @@ namespace howtouse
         {
             Assert.Equal(4, 2 + 2);
         }
+
+        [Fact]
+        //public void BadMethod()
+        //{
+        //    double result = DivideNumbers(5, 0);
+        //    Assert.Equal(double.PositiveInfinity, result);
+        //}
+        public void DivideByZeroThrowsException()
+        {
+            Assert.Throws<System.DivideByZeroException>(
+                delegate
+                {
+                    DivideNumbers(5, 0);
+                });
+        }
+
+        public int DivideNumbers(int top, int bottom)
+        {
+            return top / bottom;
+        }
     }
 }
